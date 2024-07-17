f888e9279ebf0382c90315db324b8ba3577d42f2
A91y <ayush.agr254@gmail.com>
2024-07-17 10:42:40 +0530

    refactor: Remove unused imports and fix formatting in page.tsx

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,22 +1,14 @@
-"use client";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import Form from "@/components/Form";
-import { useState } from "react";
 import Footer from "./Footer";
 
 export default function Home() {
-  const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>
-
-<div className="flex flex-col min-h-screen justify-between">
+    <div className="flex flex-col min-h-screen justify-between">
       <div> </div>
       <div className="flex justify-center items-center px-4">
         <Form />
       </div>
       <Footer />
-    
-</div>
-    </QueryClientProvider>
+    </div>
   );
 }
